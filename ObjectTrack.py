144769110571018c0cf8fe09237cc7e1685dc92e
350285449 <vladyslav.demchyk@student.tdsb.on.ca>
2024-11-08 10:43:50 -0500

    Added new changes

--- ObjectTrack.py
+++ ObjectTrack.py
@@ -66,11 +66,52 @@ def stackImages(scale,imgArray):
         for cnt in contours:
             area = cv2.contourArea(cnt)
             areaMin = cv2.getTrackbarPos("Area", "Parameters")
+
             if area > areaMin:
                 cv2.drawContours(imgContour, cnt, -1, (255, 0, 255), 7)
                 peri = cv2.arcLength(cnt, True)
                 approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
+
                 print(len(approx))
+
                 x, y, w, h = cv2.boundingRect(approx)
                 cv2.rectangle(imgContour, (x, y), (x + w, y + h), (0, 255, 0), 5)
-            break
+
+                cv2.putText(imgContour, "Points: " + str(len(approx)),
+                            (x + w + 20, y + 20), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 0), 2)
+
+                cv2.putText(imgContour, "Area: " + str(int(area)),
+                            (x + w + 20, y + 45), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 0), 2)
+
+                cv2.putText(imgContour, str(int(x)) + " : " + str(int(y)),
+                            (x - 20, y - 45), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 0), 2)
+
+                cx = int(x + (w / 2))
+                cy = int(y + (h / 2))
+
+                if cx < int(frameWidth / 2) - deadZone:
+                    cv2.putText(imgContour, "GO LEFT", (20, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 3)
+                    cv2.rectangle(imgContour, (0, int(frameHeight / 2) - deadZone),
+                                  (int(frameWidth / 2) - deadZone, int(frameHeight / 2) + deadZone),
+                                  (0, 0, 255), cv2.FILLED)
+
+                elif cx > int(frameWidth / 2) + deadZone:
+                    cv2.putText(imgContour, "GO RIGHT", (20, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 3)
+                    cv2.rectangle(imgContour, (int(frameWidth / 2) + deadZone, int(frameHeight / 2) - deadZone),
+                                  (frameWidth, int(frameHeight / 2) + deadZone),
+                                  (0, 0, 255), cv2.FILLED)
+
+                elif cy < int(frameHeight / 2) - deadZone:
+                    cv2.putText(imgContour, "GO UP", (20, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 3)
+                    cv2.rectangle(imgContour, (int(frameWidth / 2) - deadZone, 0),
+                                  (int(frameWidth / 2) + deadZone, int(frameHeight / 2) - deadZone),
+                                  (0, 0, 255), cv2.FILLED)
+
+                elif cy > int(frameHeight / 2) + deadZone:
+                    cv2.putText(imgContour, "GO DOWN", (20, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 3)
+                    cv2.rectangle(imgContour, (int(frameWidth / 2) - deadZone, int(frameHeight / 2) + deadZone),
+                                  (int(frameWidth / 2) + deadZone, frameHeight),
+                                  (0, 0, 255), cv2.FILLED)
+
+                cv2.line(imgContour, (int(frameWidth / 2), int(frameHeight / 2)), (cx, cy), (0, 0, 255), 3)
+
