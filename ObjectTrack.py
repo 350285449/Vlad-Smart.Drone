f1fb9ee1b90f8e70b706cf551ca749bbacf6611b
350285449 <vladyslav.demchyk@student.tdsb.on.ca>
2024-11-09 23:29:01 -0500

    Add image processing and contour detection code

--- ObjectTrack.py
+++ ObjectTrack.py
@@ -52,7 +52,7 @@ def stackImages(scale,imgArray):
         ver = np.vstack(hor)
     for x in range(0, rows):
         if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
-            imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)4
         else:
             imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None, scale, scale)
         if len(imgArray[x].shape) == 2:
@@ -114,4 +114,55 @@ def stackImages(scale,imgArray):
                                   (0, 0, 255), cv2.FILLED)
 
                 cv2.line(imgContour, (int(frameWidth / 2), int(frameHeight / 2)), (cx, cy), (0, 0, 255), 3)
+
+
+def display(img):
+    cv2.line(img, (int(frameWidth / 2) - deadZone, 0), (int(frameWidth / 2) - deadZone, frameHeight), (255, 255, 0), 3)
+    cv2.line(img, (int(frameWidth / 2) + deadZone, 0), (int(frameWidth / 2) + deadZone, frameHeight), (255, 255, 0), 3)
+
+    cv2.circle(img, (int(frameWidth / 2), int(frameHeight / 2)), 5, (0, 0, 255), 5)
+
+    cv2.line(img, (0, int(frameHeight / 2) - deadZone), (frameWidth, int(frameHeight / 2) - deadZone), (255, 255, 0), 3)
+    cv2.line(img, (0, int(frameHeight / 2) + deadZone), (frameWidth, int(frameHeight / 2) + deadZone), (255, 255, 0), 3)
+
+
+while True:
+    _, img = cap.read()
+    imgContour = img.copy()
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    h_min = cv2.getTrackbarPos("HUE Min", "HSV")
+    h_max = cv2.getTrackbarPos("HUE Max", "HSV")
+    s_min = cv2.getTrackbarPos("SAT Min", "HSV")
+    s_max = cv2.getTrackbarPos("SAT Max", "HSV")
+    v_min = cv2.getTrackbarPos("VALUE Min", "HSV")
+    v_max = cv2.getTrackbarPos("VALUE Max", "HSV")
+
+    print(h_min)
+
+    lower = np.array([h_min, s_min, v_min])
+    upper = np.array([h_max, s_max, v_max])
+
+    mask = cv2.inRange(imgHSV, lower, upper)
+    result = cv2.bitwise_and(img, img, mask=mask)
+    mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+
+    imgBlur = cv2.GaussianBlur(result, (7, 7), 1)
+    imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
+
+    threshold1 = cv2.getTrackbarPos("Threshold1", "Parameters")
+    threshold2 = cv2.getTrackbarPos("Threshold2", "Parameters")
+    imgCanny = cv2.Canny(imgGray, threshold1, threshold2)
+
+    kernel = np.ones((5, 5))
+    imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
+
+    getContours(imgDil, imgContour)
+    display(imgContour)
+
+    stack = stackImages(0.7, ([img, result], [imgDil, imgContour]))
+
+    cv2.imshow("Horizontal Stacking", stack)
+
+
 
